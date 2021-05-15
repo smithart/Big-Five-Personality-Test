09ff31772180a43b37986b403e99ddbbb8f9d24c
Brad Smithart <brad@smithart.net>
2021-05-14 22:05:54 -0500

    Rebranding as FRAME test

--- js/main.js
+++ js/main.js
@@ -18,18 +18,23 @@ function scoreTest(event) {
 	event.preventDefault();
 
 	// Calculate the score for the test
+	// Adventurous/Openness (10)
 	var oResult = 8 + Number(form.elements["q5"].value) - Number(form.elements["q10"].value) + Number(form.elements["q15"].value) - Number(form.elements["q20"].value) + Number(form.elements["q25"].value) - Number(form.elements["q30"].value) + Number(form.elements["q35"].value) + Number(form.elements["q40"].value) + Number(form.elements["q45"].value) + Number(form.elements["q50"].value);
+	// Reliable/Conscientiousness (10)
 	var cResult = 14 + Number(form.elements["q3"].value) - Number(form.elements["q8"].value) + Number(form.elements["q13"].value) - Number(form.elements["q18"].value) + Number(form.elements["q23"].value) - Number(form.elements["q28"].value) + Number(form.elements["q33"].value) - Number(form.elements["q38"].value) + Number(form.elements["q43"].value) + Number(form.elements["q48"].value);
+	// Extroverted/Extroversion (10)
 	var eResult = 20 + Number(form.elements["q1"].value) - Number(form.elements["q6"].value) + Number(form.elements["q11"].value) - Number(form.elements["q16"].value) + Number(form.elements["q21"].value) - Number(form.elements["q26"].value) + Number(form.elements["q31"].value) - Number(form.elements["q36"].value) + Number(form.elements["q41"].value) - Number(form.elements["q46"].value);
+	// Flexible/Agreeableness (10)
 	var aResult = 14 - Number(form.elements["q2"].value) + Number(form.elements["q7"].value) - Number(form.elements["q12"].value) + Number(form.elements["q17"].value) - Number(form.elements["q22"].value) + Number(form.elements["q27"].value) - Number(form.elements["q32"].value) + Number(form.elements["q37"].value) + Number(form.elements["q42"].value) + Number(form.elements["q47"].value);
-	var nResult = 38 - Number(form.elements["q4"].value) + Number(form.elements["q9"].value) - Number(form.elements["q14"].value) + Number(form.elements["q19"].value) - Number(form.elements["q24"].value) - Number(form.elements["q29"].value) - Number(form.elements["q34"].value) - Number(form.elements["q39"].value) - Number(form.elements["q44"].value) - Number(form.elements["q49"].value);
+	// Mature/non-Neuroticism (10) -- reverse +/-
+	var nResult = 2 + Number(form.elements["q4"].value) - Number(form.elements["q9"].value) + Number(form.elements["q14"].value) + Number(form.elements["q19"].value) - Number(form.elements["q24"].value) + Number(form.elements["q29"].value) + Number(form.elements["q34"].value) + Number(form.elements["q39"].value) + Number(form.elements["q44"].value) + Number(form.elements["q49"].value);
 
 	// Add the test results to the score graph
-	oBarCaption.textContent = "Openness: " + oResult;
-	cBarCaption.textContent = "Conscientiousness: " + cResult;
-	eBarCaption.textContent = "Extraversion: " + eResult;
-	aBarCaption.textContent = "Agreeableness: " + aResult;
-	nBarCaption.textContent = "Neuroticism: " + nResult;
+	oBarCaption.textContent = "Adventurous: " + oResult;
+	cBarCaption.textContent = "Reliable: " + cResult;
+	eBarCaption.textContent = "Extroverted: " + eResult;
+	aBarCaption.textContent = "Flexible: " + aResult;
+	nBarCaption.textContent = "Mature: " + nResult;
 }
 
 form.onsubmit = scoreTest
---
+++ questions.js
@@ -0,0 +1,13 @@
+/*
+q_num/id
+option
+value
+
+
+*/
+/*
+q1
+radio5q1
+value
+
+*/
